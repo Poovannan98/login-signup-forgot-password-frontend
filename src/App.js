92e8429a962f41a255c2bfe25275767a0665f389
Poovannan98 <poovannan098@gmail.com>
2022-11-22 12:07:49 +0530

    add email verify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./Components/Main";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import EmailVerify from "./Components/EmailVerify";
+import ForgotPassword from "./Components/ForgotPassword";
+import PasswordReset from "./Components/PasswordReset";
 
 function App() {
   const user = localStorage.getItem("token");
@@ -12,6 +15,9 @@ function App() {
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+			<Route path="/forgot-password" element={<ForgotPassword />} />
+			<Route path="/password-reset/:id/:token" element={<PasswordReset />} />
 		</Routes>
 	);
 };
