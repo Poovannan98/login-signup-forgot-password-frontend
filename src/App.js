4461e9ac28615240766566d56020d5c352cb89bd
Poovannan98 <poovannan098@gmail.com>
2022-11-29 13:40:24 +0530

    link verify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./Components/Main";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
-// import EmailVerify from "./Components/EmailVerify";
+import EmailVerify from "./Components/EmailVerify";
 import ForgotPassword from "./Components/ForgotPassword";
 import PasswordReset from "./Components/PasswordReset";
 
@@ -15,7 +15,7 @@ function App() {
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
-			{/* <Route path="/users/:id/verify/:token" element={<EmailVerify />} /> */}
+			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
 			<Route path="/forgot-password" element={<ForgotPassword />} />
 			<Route path="/password-reset/:id/:token" element={<PasswordReset />} />
 		</Routes>
